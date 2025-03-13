7fdd9b34e18f96031d3cfae37205559ba8adbd3b
franciscoL0pez <franlopez.2001@hotmail.com>
2025-03-13 09:43:03 -0300

    Connect with db (this time well done)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-const  express = require('express');
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
 const app = express();
+
+//routes 
+app.get('/', (req, res) => {
+  res.send('Welcome to my api');
+});
+
+
+//mongo db connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('DB connected'))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
